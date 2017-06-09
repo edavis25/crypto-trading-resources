6ff2fc580db9bd931cc9f2a6438bd341a5a405cc
edavis25 <davis.25811@gmail.com>
2017-06-08 21:53:37 -0400

    Added functions for margin trading, retrieving historic data, and retrieving balances. Also added readme.

--- poloniex/Poloniex.php
+++ poloniex/Poloniex.php
@@ -3,9 +3,12 @@
 	// FINAL TESTED CODE - Created by Compcentral (source: https://pastebin.com/iuezwGRZ)
 
 	// Eric's Note: All functions created by above author EXCEPT:
-		// 1. 
-		// 2.
-		// 3.
+		// 1. All margin trading functions
+		// 2. All lending functions
+		// 3. get_trade_history()
+		// 4. get_chart_data()
+		// 5. get_currency_data()
+		// 6. get_balances() + get_available_balances()
 
 	// NOTE: currency pairs are reverse of what most exchanges use...
 	// For instance, instead of XPM_BTC, use BTC_XPM
@@ -33,9 +36,34 @@ class poloniex {
 	 * URL GET requests not mapped to account actions 
 	 ************************************************************/ 
 
-	public function get_trade_history($pair) {
-		$trades = $this->retrieveJSON($this->public_url.'?command=returnTradeHistory&currencyPair='.strtoupper($pair));
-		return $trades;
+	// Max 1 year or API throws error & returns max of 50000 trades
+	// Returns most recent 200 trades if start/end date arent specified
+	public function get_trade_history($pair, $start_date = false, $end_date = false) {
+		// Build URL
+		$url = $this->public_url . '?command=returnTradeHistory';
+		$url .= '&currencyPair=' . strtoupper($pair);
+
+		// Add start and end UNIX timestamps
+		if ($start_date && $end_date) {
+			$url .= '&start=' . $start_date;
+			$url .= '&end=' . $end_date;
+		}
+
+		return $this->retrieveJSON($url);
+	}
+
+
+	// Timeperiod = candlestick period in seconds. Default value = 5 mins
+	// Valid periods = 300, 900, 1800, 7200, 14400, 86400
+	public function get_chart_data($pair, $start_date, $end_date = 9999999999, $time_period = 300) {
+		// Build URL
+		$url = $this->public_url . '?command=returnChartData';
+		$url .= '&currencyPair=' . strtoupper($pair);
+		$url .= '&start=' . $start_date;
+		$url .= '&end=' . $end_date;
+		$url .= '&period=' . $time_period;
+
+		return $this->retrieveJSON($url);
 	}
 
 
@@ -45,12 +73,14 @@ class poloniex {
 	}
 
 
+	// Get past 24 hour volume
 	public function get_volume() {
 		$volume = $this->retrieveJSON($this->public_url.'?command=return24hVolume');
 		return $volume;
 	}
 
 
+	// Get ticker info for pair
 	public function get_ticker($pair = "ALL") {
 		$pair = strtoupper($pair);
 		$prices = $this->retrieveJSON($this->public_url.'?command=returnTicker');
@@ -71,28 +101,79 @@ class poloniex {
 	}
 
 
+	// Get simple list of trading pairs (ex: BTC_ETH, BTC_LTC, BTC_DOGE, etc.)
 	public function get_trading_pairs() {
 		$tickers = $this->retrieveJSON($this->public_url.'?command=returnTicker');
 		return array_keys($tickers);
 	}
 
 
+	// Return currency info (min confirms/full names/disabled or delisted/etc.)
+	public function get_currency_data($currency = 'all') {
+		$url = $this->public_url . '?command=returnCurrencies';
+		$data = $this->retrieveJSON($url);
+
+		if ($currency == 'all') {
+			return $data;
+		}
+		else {
+			$currency = strtoupper($currency);
+			if (isset($data[$currency])) {
+				return $data[$currency];
+			}
+			else {
+				return array();
+			}
+		}
+
+
+	}
+
+
 
 	/************************************************************
-	 *	Account Execution Functions (trades/balances/etc.)
+	 *	Account Execution Trading Functions (trades/balances/etc.)
 	 *  POST requests for specific account info & actions
 	 ************************************************************/ 
 
 	/*
 	 *  Functions for account info (balances/history/orders/etc.):
 	 */
-	public function get_balances() {
-		return $this->query( 
+	
+	// Get current balances for coins (including 0 balances)
+	// Note: Does not retrieve balances in margin account
+	public function get_balances($currency = 'all') {
+		$data = $this->query( 
 			array(
 				'command' => 'returnBalances'
 			)
 		);
+
+		if ($currency == 'all') {
+			return $data;						// Return all balances
+		}
+
+		$currency = strtoupper($currency);
+		if (isset($data[$currency])) {			
+			return $data[$currency];			// Return specified balance
+		}
+		else {
+			return array();						// Currency not found, return empty array
+		}
 	}
+
+
+	// Get available balances from all areas of account (non-zero balances)
+	// Note: Balances returned as array(3) - keys = 'exchange' 'margin' 'lending'
+	public function get_available_balances() {
+		$data = array(
+			'command' => 'returnAvailableAccountBalances'
+		);
+		return $this->query($data);
+	}
+
+
+
 
 	public function get_open_orders($pair) {		
 		return $this->query( 
@@ -150,7 +231,7 @@ class poloniex {
 
 
 	/*
-	 *  Functions for basic trade execution:
+	 *  Functions for standard trade execution:
 	 */
 	public function buy($pair, $rate, $amount) {
 		return $this->query( 
@@ -177,7 +258,7 @@ class poloniex {
 	public function cancel_order($pair, $order_number) {
 		return $this->query( 
 			array(
-				'command' => 'cancelOrder',	
+				'command' => 'cancelOrder',	 
 				'currencyPair' => strtoupper($pair),
 				'orderNumber' => $order_number
 			)
@@ -189,23 +270,65 @@ class poloniex {
 	 *  Functions for margin trading
 	 */
 
-	// Get current open 
+	// Get current margin positions info
 	public function get_margin_position($pair = "all") {
-    	$arr = array(
+    	$data = array(
 			'command' => 'getMarginPosition',
 			'currencyPair' => strtoupper($pair)
         );
-        return $this->query($arr); 
+        return $this->query($data); 
+    }
+
+    // Get meta summary of margin account (fees/borrowed value/etc.)
+    public function get_margin_account_summary() {
+    	$data = array(
+    		'command' => 'returnMarginAccountSummary'
+    	);
+    	return $this->query($data);
+    }
+
+    // Close a margin position (returns success even if no position is currently open)
+    public function close_margin_position($pair) {
+    	$data = array(
+    		'command' => 'closeMarginPosition',
+    		'currencyPair' => strtoupper($pair)
+    	);
+    	return $this->query($data);
+    }
+
+    // Default max lending rate = 2%
+    public function margin_buy($rate, $amount, $pair, $max_lending_rate = 2) {
+    	$data = array(
+    		'command' => 'marginBuy',
+    		'rate' => $rate,
+    		'amount' => $amount,
+    		'currencyPair' => strtoupper($pair),
+    		'lendingRate' => $max_lending_rate
+    	);
+    	return $this->query($data);
+    }
+
+    // Default max lending rate = 2%
+    public function margin_sell($rate, $amount, $pair, $max_lending_rate = 2) {
+    	$data = array(
+    		'command' => 'marginSell',
+    		'rate' => $rate,
+    		'amount' => $amount,
+    		'currencyPair' => strtoupper($pair),
+    		'lendingRate' => $max_lending_rate
+    	);
+    	return $this->query($data);
     }
 
 
-
-
+    /*
+	 *  Functions for lending
+	 */
 
 
 
     /*
-     *  Functions for account balances (withdraws/etc.)
+     *  Functions for addresses and withdraws
      */
 	public function withdraw($currency, $amount, $address) {
 		return $this->query( 
