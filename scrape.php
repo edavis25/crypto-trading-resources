4bf687c02b600b9c0b737567f39793f733425a50
Eric Davis <davis.25811@gmail.com>
2017-06-14 02:57:59 -0400

    Changed inserts into batches.

--- scrape.php
+++ scrape.php
@@ -3,20 +3,34 @@
 require 'poloniex/Poloniex.php';
 require 'influxdb/InfluxDB.php';
 
-set_time_limit(7200);
+const FIVE_MINUTES = 300;
+const TWO_HOURS = 7200;
+const MAX_INSERT_BATCH = 6000;
+
+set_time_limit(TWO_HOURS);
 ini_set('memory_limit','512M');
+
 
 
 $polo = new Poloniex($key, $secret);
 $db = new InfluxDB('poloniex', '192.168.0.101');
 
+$start = time();
+echo "START TIME: " . $start . "<br>";
+
 // Get trading pairs
 //$pairs = $polo->get_trading_pairs();
 
-$data = $polo -> get_chart_data('btc_doge', 1405699200, (1405699200+10000), 300);
+$data = $polo -> get_chart_data('btc_doge', 0000000000, 9999999999, FIVE_MINUTES);
 
-$insert = array();
+echo "NUMBER RESULTS: " . count($data) . "<br>";
+
+// Create insert data array
+$insert_data = array();
+$insert_count = 0;
+$i = 0;
 foreach ($data as $row) {
+	// Format string w/ Influx line protocol
 	$str = "BTC_DOGE ";
 	$str .= "high=" . format_num($row['high']) . ',';
 	$str .= "low=" . format_num($row['low']) . ',';
@@ -27,10 +41,28 @@ foreach ($data as $row) {
 	$str .= "weighted_average=" . format_num($row['weightedAverage']);
 	$str .= ' ' . $row['date'];
 
-	echo $str . "<br>";
+	$insert_data[] = $str;
+	$i += 1;
+
+	// Insert batches
+	if ($i == MAX_INSERT_BATCH - 1) {
+		$db -> insert($insert_data, 's');
+
+		$insert_count += count($insert_data);
+		$insert_data = array();
+		$i = 0;
+	}
 }
 
+echo "NUMBER INSERTS: " . (count($insert_data) + $insert_count) . "<br>";
 
+// Insert the rest into database
+$db -> insert($insert_data, 's');
+
+echo "DONE!<br>";
+$end = time();
+echo "END TIME: " . $end . "<br>";
+echo "ELAPSED: " . ($end - $start);
 
 function format_num($num) {
 	return number_format($num, 8, '.', '');
